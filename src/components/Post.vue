0bc082e585b0c2b17197a584a8ca7f615aa79de6
Erik <erikhau3010@gmail.com>
2021-08-25 22:39:07 +0700

    initial commit

---
+++ src/components/Post.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="post-container" v-if="post">
+    <div class="post-title">
+      <h3>{{ post.title }}</h3>
+    </div>
+    <div class="post-content">
+      <p>{{ post.content }}</p>
+    </div>
+    <div class="post-comments">
+      <h4 class="post-comments-title">Comments</h4>
+      <Comment />
+    </div>
+  </div>
+</template>
+
+<script>
+import Comment from "@/components/Comment";
+
+export default {
+  name: "Post",
+  components: {
+    Comment,
+  },
+  props: ["post"],
+};
+</script>
+
+<style lang="scss" scoped>
+.post-container {
+  border: 1px solid #c9c9c9;
+  border-radius: 5px;
+  padding: 1.25rem;
+
+  .post-title {
+    h3 {
+      font-size: 1.2rem;
+    }
+  }
+
+  .post-content {
+    margin-top: 0.35rem;
+    p {
+      font-size: 0.85rem;
+      color: #868686;
+    }
+  }
+
+  .post-comments {
+    margin-top: 2rem;
+    &-title {
+      margin-bottom: 0.15rem;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <Post :post="post" />
+  </div>
+</template>
+
+<script>
+import Post from "@/components/Post";
+
+import http from "@/http";
+
+import { ref, onMounted } from "vue";
+
+export default {
+  name: "Home",
+  components: {
+    Post,
+  },
+  setup() {
+    const post = ref(null);
+
+    const fetchPost = async () => {
+      const result = await http.get("post/13");
+
+      post.value = result.data;
+
+      console.log(result.data);
+    };
+
+    onMounted(() => {
+      fetchPost();
+    });
+
+    return { fetchPost, post };
+  },
+};
+</script>
+
+<style lang="scss"></style>
